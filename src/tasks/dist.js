abb066fbc39ee1748ed5b0913410b23bf9e22add
Hampus Persson <h@hmps.se>
2015-04-07 14:13:03 +0200

    move bump task from dist file to release file

--- src/tasks/dist.js
+++ src/tasks/dist.js
@@ -1,23 +1,3 @@
 import gulp from 'gulp';
-import bump from 'gulp-bump';
 
-gulp.task('build', ['build:js', 'styles:min'], function gulpBuild() {});
-
-gulp.task('bump', function() {
-    bumpHelper({});
-});
-
-gulp.task('bump:minor', function() {
-    bumpHelper({type: 'minor'});
-});
-
-gulp.task('bump:major', function() {
-    bumpHelper({type: 'major'});
-});
-
-function bumpHelper(_options) {
-    return gulp.src('./package.json')
-      .pipe(bump(_options))
-      .pipe(gulp.dest('./'));
-}
-
+gulp.task('dist', ['clean:dist', 'js:dist', 'styles:dist', 'images:dist', 'html:dist'], () => {});
--- src/tasks/release.js
+++ src/tasks/release.js
@@ -1,45 +1,63 @@
 import gulp from 'gulp';
-import header from 'header';
-import size from 'size';
-import pkg from 'pkg';
+import header from 'gulp-header';
+import size from 'gulp-size';
+import bump from 'gulp-bump';
 
-gulp.task('release', ['build:js', 'styles:min'], function gulpBuild() {
+// Since this is dynamic we need to use a regular old require here
+var pkg = require(process.cwd() + '/package.json');
+
+// git tag
+// git commit
+
+gulp.task('release', () => {
     var banner = getBannerText();
 
-    gulp.src('build/*.js')
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('dist'))
-        .pipe(size());
-
-    gulp.src('build/javascript/*.js')
-        .pipe(gulp.dest('dist/javascript'))
-        .pipe(size());
-
-    gulp.src('build/stylesheets/*.min.css')
+    gulp.src('dist/stylesheets/*.css')
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('dist/stylesheets'))
         .pipe(size());
 
-    gulp.src('src/templates/*.html')
-        .pipe(gulp.dest('dist/templates'))
+    gulp.src('dist/*.module.js')
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist'))
         .pipe(size());
 
-    gulp.src([
-            'src/images/**/*.png',
-            'src/images/**/*.gif',
-            'src/images/**/*.jpg',
-            'src/images/**/*.jpeg',
-            'src/images/**/*.webp',
-            'src/images/**/*.svg'
-        ])
-        .pipe(gulp.dest('dist/images'))
-        .pipe(size());
 });
 
+gulp.task('bump', function() {
+    bumpHelper({});
+});
+
+    gulp.task('bump:patch', function() {
+        bumpHelper({});
+    });
+
+    gulp.task('bump:minor', function() {
+        bumpHelper({type: 'minor'});
+    });
+
+    gulp.task('bump:major', function() {
+        bumpHelper({type: 'major'});
+    });
+
+    function bumpHelper(_options) {
+        return gulp.src('./package.json')
+          .pipe(bump(_options))
+          .pipe(gulp.dest('./'));
+    }
+
+
+/**
+ * Return a formatted string to put in the banner.
+ * @return {[type]} [description]
+ *
+ * @todo Check that all params exist in package.json
+ */
 function getBannerText() {
     return `/**,
 * <%= pkg.name %> - <%= pkg.description %>,
 * @version v<%= pkg.version %>,
 * @author  <%= pkg.author %>,
-*/`;
+*/
+`;
 }
